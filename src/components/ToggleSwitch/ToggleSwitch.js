a9e638a6227381a4345ef138c565ecba4b0c0c4c
kunalmeon <kunal.neeon@gmail.com>
2026-04-01 14:21:53 +1030

    update

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,27 +1,52 @@
-import React, {useState, useContext} from "react";
+// import React, {useState, useContext} from "react";
+// import emoji from "react-easy-emoji";
+// import StyleContext from "../../contexts/StyleContext";
+// import "./ToggleSwitch.scss";
+
+// const ToggleSwitch = () => {
+//   const {isDark} = useContext(StyleContext);
+//   const [isChecked, setChecked] = useState(isDark);
+//   const styleContext = useContext(StyleContext);
+
+//   return (
+//     <label className="switch">
+//       <input
+//         type="checkbox"
+//         checked={isDark}
+//         onChange={() => {
+//           styleContext.changeTheme();
+//           setChecked(isChecked);
+//         }}
+//       />
+//       <span className="slider round">
+//         <span className="emoji">{isChecked ? emoji("🌜") : emoji("☀️")}</span>
+//       </span>
+//     </label>
+//   );
+// };
+// export default ToggleSwitch;
+
+
+import React, { useContext } from "react";
 import emoji from "react-easy-emoji";
 import StyleContext from "../../contexts/StyleContext";
 import "./ToggleSwitch.scss";
 
 const ToggleSwitch = () => {
-  const {isDark} = useContext(StyleContext);
-  const [isChecked, setChecked] = useState(isDark);
-  const styleContext = useContext(StyleContext);
+  const { isDark, changeTheme } = useContext(StyleContext);
 
   return (
     <label className="switch">
       <input
         type="checkbox"
-        checked={!isDark}
-        onChange={() => {
-          styleContext.changeTheme();
-          setChecked(isChecked);
-        }}
+        checked={isDark} // checked = dark mode ON
+        onChange={changeTheme} // toggle theme
       />
       <span className="slider round">
-        <span className="emoji">{!isChecked ? emoji("🌜") : emoji("☀️")}</span>
+        <span className="emoji">{isDark ? emoji("🌜") : emoji("☀️")}</span>
       </span>
     </label>
   );
 };
+
 export default ToggleSwitch;
